5c9150b73289c600a1f09058cdef1867dde4e966
himynameisriz <v.rizwanali@gmail.com>
2017-04-10 16:31:29 -0500

    Adding in the stuff for gopkg, github, and goji.

--- main.go
+++ main.go
@@ -1,1 +1,35 @@
-package resumeAPI
+package main
+// +build ignore
+import (
+	"gopkg.in/mgo.v2"
+	"fmt"
+	//"gopkg.in/mgo.v2/bson"
+)
+
+type BasicDetails struct {
+	coll *mgo.Collection
+}
+
+func main() {
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+	db := mgo.Database{}
+	db.Name = "test"
+	db.Session = session
+
+	collections, _ := db.CollectionNames()
+
+	//collection := db.C(collections[0])
+
+	//collection.
+
+
+	fmt.Println(collections)
+	//mux := goji.NewMux()
+	//http.ListenAndServe("google.com", )
+}
